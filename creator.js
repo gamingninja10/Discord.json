e4f449dfb8e535187aab0d550a222bd202a69b13
DotCD <dotcdtg@gmail.com>
2018-09-24 15:18:18 -0400

    Added cayman style css to generator page

--- creator.js
+++ creator.js
@@ -17,7 +17,7 @@ function commandEditor()
     input.size = 8;
 
     var argAmount = document.createElement("input");
-    argAmount.classList.add("right-spacer");
+    argAmount.classList.add("spacer-right");
     argAmount.type = "number";
     argAmount.min = 0;
     argAmount.max = 9;
@@ -25,6 +25,7 @@ function commandEditor()
     argAmount.className = "argCountInput"
 
     var addAction = document.createElement("button");
+    argAmount.classList.add("spacer-right");
     addAction.textContent = "+";
     addAction.setAttribute("onClick", "actionEditor(\""+id+"\")");
 
